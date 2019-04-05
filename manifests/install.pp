fb0b5727fcf8d2959bda80ee0ec3cf55c2e1b31b
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2019-04-05 08:56:41 +0200

    let the $git_url parameter do what's intended to do and don't use the hardcoded url to my repo

--- manifests/install.pp
+++ manifests/install.pp
@@ -30,49 +30,52 @@ class gitlab_runner::install (
     group   => $group,
     require => User[$user],
   }
-  File { "${home}/Go/src/github.com":
+
+  $pathelems = split($git_url, '/')
+
+  File { "${home}/Go/src/${pathelems[2]}":
     ensure  => 'directory',
     owner   => $user,
     group   => $group,
     require => User[$user],
   }
-  File { "${home}/Go/src/github.com/buzzdeee":
+  File { "${home}/Go/src/${pathelems[2]}/${pathelems[3]}":
     ensure  => 'directory',
     owner   => $user,
     group   => $group,
     require => User[$user],
   }
 
-  vcsrepo { "${home}/Go/src/github.com/buzzdeee/gitlab-runner":
-    source   => 'https://github.com/buzzdeee/gitlab-runner.git',
+  vcsrepo { "${home}/Go/src/${pathelems[2]}/${pathelems[3]}/${pathelems[4]}":
+    source   => $git_url,
     user     => $user,
     provider => 'git',
-    require  => File["${home}/Go/src/github.com/buzzdeee"],
+    require  => File["${home}/Go/src/${pathelems[2]}/${pathelems[3]}"],
   }
 
   exec { 'install_runner_deps':
-    cwd         => "${home}/Go/src/github.com/buzzdeee/gitlab-runner",
+    cwd         => "${home}/Go/src/${pathelems[2]}/${pathelems[3]}/${pathelems[4]}",
     user        => $user,
     command     => 'gmake deps',
     environment => [ "PATH=${home}/Go/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11R6/bin:/usr/local/sbin",
                      "GOPATH=${home}/Go", ],
-    creates     => "${home}/Go/src/github.com/buzzdeee/gitlab-runner/.gopath/bin/mockery",
+    creates     => "${home}/Go/src/${pathelems[2]}/${pathelems[3]}/${pathelems[4]}/.gopath/bin/mockery",
     timeout     => 2000,
-    require     => Vcsrepo["${home}/Go/src/github.com/buzzdeee/gitlab-runner"],
+    require     => Vcsrepo["${home}/Go/src/${pathelems[2]}/${pathelems[3]}/${pathelems[4]}"],
   }
   exec { 'build_runner':
-    cwd         => "${home}/Go/src/github.com/buzzdeee/gitlab-runner",
+    cwd         => "${home}/Go/src/${pathelems[2]}/${pathelems[3]}/${pathelems[4]}",
     user        => $user,
     command     => 'gmake install',
     environment => [ "PATH=${home}/Go/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11R6/bin:/usr/local/sbin",
                      "GOPATH=${home}/Go", ],
     timeout     => 2000,
-    creates => "${home}/Go/src/github.com/buzzdeee/gitlab-runner/.gopath/bin/gitlab-runner",
+    creates => "${home}/Go/src/${pathelems[2]}/${pathelems[3]}/${pathelems[4]}/.gopath/bin/gitlab-runner",
     require     => Exec['install_runner_deps'],
   }
   exec { 'install_runner':
     cwd     => "${home}/Go",
-    command => "/usr/bin/install -o root -g bin -m 0755 src/github.com/buzzdeee/gitlab-runner/.gopath/bin/gitlab-runner /usr/local/bin/gitlab-runner",
+    command => "/usr/bin/install -o root -g bin -m 0755 src/${pathelems[2]}/${pathelems[3]}/${pathelems[4]}/.gopath/bin/gitlab-runner /usr/local/bin/gitlab-runner",
     creates => '/usr/local/bin/gitlab-runner',
     require => Exec['build_runner'],
   }
